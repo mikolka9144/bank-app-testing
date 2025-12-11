75078daf471ed8ca2f565d80aec4262dc56772ba
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2025-12-11 12:13:57 +0100

    remove a file

--- tests/api/test_api_mf.py
+++
@@ -1,9 +0,0 @@
-from src.account import CompanyAccount
-import pytest
-
-class MFTests:
-
-    def test_nip_valid(self):
-        acc = Account("John","Doe","32111111111")
-        acc.balance = 100
-        return acc
