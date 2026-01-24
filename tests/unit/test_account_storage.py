6ee3130adf0e579de2ded3aeea1d2569f4b121a5
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2026-01-24 15:18:30 +0100

    fix coverage

--- tests/unit/test_account_storage.py
+++ tests/unit/test_account_storage.py
@@ -12,9 +12,22 @@ class TestCompanyAccount:
         ext = self.account1.to_dict()
         assert ext["first_name"] == "John"
         assert ext["last_name"] == "Doe"
-    def test_account_depacking(self):
+    def test_base_account_packing(self):
+        ext = BaseAccount().to_dict()
+        assert ext["balance"] == 0
+        assert ext["history"] == []
+    def test_base_account_depacking(self):
         ext = BaseAccount.from_dict({
             "balance":100,
             "history":[]
+        })
+        assert ext.balance == 100
+    def test_account_depacking(self):
+        ext = Account.from_dict({
+            "balance":100,
+            "history":[],
+            "first_name":"test",
+            "last_name":"Dog",
+            "pesel":"Invalid"
         })
         assert ext.balance == 100
